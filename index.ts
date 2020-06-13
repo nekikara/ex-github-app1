6bea83569ec5318fc5360a5de94572ad8f33c94b
nekikara <nekikara@gmail.com>
2020-06-13 20:12:21 +0200

    Create a fixed issue

--- index.ts
+++ index.ts
@@ -1,1 +1,46 @@
-console.log('hogehoge')
+const fs = require('fs')
+import { App } from "@octokit/app"
+import { request } from "@octokit/request"
+import dotenv from 'dotenv'
+dotenv.config()
+
+async function main() {
+  const APP_ID = Number(process.env.APP_ID!)
+  const owner = process.env.OWNER!
+  const repo = process.env.REPO!
+  const PEM = fs.readFileSync('private-key.pem', 'utf8');
+
+  const app = new App({id: APP_ID, privateKey: PEM});
+  const jwt = app.getSignedJsonWebToken();
+
+  const installationOption =
+  {
+    owner,
+    repo,
+    headers: {
+      authorization: `Bearer ${jwt}`,
+      accept: "application/vnd.github.machine-man-preview+json",
+    },
+  }
+  // @ts-ignore
+  const { data } = await request("GET /repos/:owner/:repo/installation", installationOption);
+
+  const installationId = data.id
+  const installationAccessToken = await app.getInstallationAccessToken({
+    installationId
+  })
+
+  const options = {
+    owner,
+    repo,
+    headers: {
+      authorization: `token ${installationAccessToken}`,
+      accept: "application/vnd.github.machine-man-preview+json",
+    },
+    title: 'Second Issue',
+  }
+  const x = await request("POST /repos/:owner/:repo/issues", options)
+  console.log('result: ', x)
+}
+
+main().catch((e) => {console.log('error', e)})
